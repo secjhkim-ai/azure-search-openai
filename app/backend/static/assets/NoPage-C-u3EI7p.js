3802b864e68573973367b5c0d940570e4e43c0d0
s_Kim <s_jeonghoon@yulchon.com>
2025-08-14 15:19:18 +0900

    Add frontend+backend integrated Azure deploy workflow for LawGIA

---
+++ app/backend/static/assets/NoPage-C-u3EI7p.js
@@ -0,0 +1,2 @@
+import{bH as o}from"./vendor-Db6cl1CV.js";function e(){return o.jsx("h1",{children:"404"})}e.displayName="NoPage";export{e as Component};
+//# sourceMappingURL=NoPage-C-u3EI7p.js.map
